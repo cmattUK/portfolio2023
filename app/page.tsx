24d07af76ff003d83fbf3cce584622ec2dc8420c
cmattUK <chris.elixir@gmail.com>
2023-08-20 13:53:23 +0100

    removed old preloader that was acausing conflict and added a new

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Lines } from 'react-preloaders';
 import Nav from '../components/Nav';
 import Hero from '../components/Hero';
 import About from '../components/About';
@@ -8,20 +7,50 @@ import Work from '../components/Work';
 import VideoWork from '../components/Video';
 import More from '../components/More';
 import Quotes from '../components/Quotes';
+import {useState, useEffect} from 'react';
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 
 export default function Home() {
 
+  const [loading, setLoading] = useState(false);
+  const [hasLoaded, setLoaded] = useState(false);
+  let [color, setColor] = useState("#9f1239");
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoaded(true);
+      setTimeout(() => {
+        setLoading(false);
+    }, 300);
+    }, 2700);
+  }, []); 
+
   return (
     <main className="border-t-4 border-rose-600">
-      <Lines color={'#9f1239'} />
-      <Nav/>
-      <Hero/>
-      <About/>
-      <Work/>
-      <VideoWork/>
-      <More/>
-      <Quotes/>
+      {loading ? 
+      (
+        <div className={`flex justify-center items-center  h-screen ${hasLoaded ? 'transition ease-out duration-300 translate-y-full':'translate-y-0'}`}>
+          <ScaleLoader
+            color={color}
+            loading={loading}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : 
+      (
+        <div>
+        <Nav/>
+        <Hero/>
+        <About/>
+        <Work/>
+        <VideoWork/>
+        <More/>
+        <Quotes/>
+        </div>
+      )}
     </main>
   )
 }
